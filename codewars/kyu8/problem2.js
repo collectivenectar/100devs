445cc28c6f7b573e5f5a9c3f6586ce9fb7d06e6f
Jonathan Harvey <jonathanharvey22@gmail.com>
2022-06-05 00:44:08 +0200

    added quick 8 kyu problem

---
+++ codewars/kyu8/problem2.js
@@ -0,0 +1,11 @@
+// if the number is odd, return that number multiplied by 9,
+// else return multiplied by 8
+
+function simpleMultiplication(number) {
+    if(number % 2 === 0){
+      return number * 8
+    }
+  else{
+    return number * 9
+  }
+}
