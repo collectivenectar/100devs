c29252811de15bc768949f3942d954fba0c63f99
Jonathan Harvey <jonathanharvey22@gmail.com>
2022-08-22 22:05:41 +0200

    added kyu5 with PREP

---
+++ codewars/kyu5/problem20.js
@@ -0,0 +1,58 @@
+// Parameters: given a series of cardinal directions, as an array of strings, each element
+// either upper completel lower case or first letter capitalized.
+
+// Return: Return the directions with all the unnecessary directions removed. To explain:
+// If there are two elements with the directions "east" and "west" right next to each other,
+// remove them from the array. The same with "north" and "south". Dont forget that after
+// You remove two elements, you may find more pairs that are now adjacent to each other
+// that weren't before.
+
+// Example:
+// dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]) => "WEST"
+
+// Pseudocode:
+
+// Best solution I could think of was recursive. Each time I'll need to search through
+// the entire array, and the moment I find one, stop - remove that pair, and start
+// over again looking through a new array copy that has the pair already removed.
+
+// Starting with the directions, I just enumerated them with negative and positive
+// values of 1 or 2 so they can be compared. If by adding them together the result
+// is 0, then they are opposite directions.
+
+// Then begin the recursive search.
+
+function dirReduc(arr){
+  let dir = {
+    "north": 1,
+    "south": -1,
+    "east": 2,
+    "west": -2
+  }
+  let recurse = (array) => {
+    let toRemove = []
+    for(let each = 0; each < array.length - 1; each++){
+      let curr = array[each].toLowerCase()
+      let next = array[each + 1].toLowerCase()
+      if(dir[curr] + dir[next] === 0){
+        toRemove.push(each)
+        toRemove.push(each + 1)
+        break
+      }
+    }
+    if(toRemove.length > 0){
+      let newArray = array.filter((el, index) => {
+        if(index !== toRemove[0] && index !== toRemove[1]){
+          return el
+        }
+      })
+      return recurse(newArray)
+    }else{
+      return array
+    }
+  }
+  return recurse(arr)
+}
+
+let test = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+dirReduc(test)
